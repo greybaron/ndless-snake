30113f591d2dc6f85879baa5c509d019ae81ab1a
Florian Schmidt <flo.schmidt10@gmail.com>
2023-09-19 13:29:11 +0200

    hessenberg

--- src/main.rs
+++ src/main.rs
@@ -26,52 +26,292 @@ struct Cell {
     y: i16,
 }
 
-fn main() {
-    // screen setup
-    let screen = ndless_sdl::init_default().expect("failed to set video mode");
+type Mat = [[f32; 4]; 4];
 
-    // used for score, color indicates difficulty
-    let fonts = vec![
-        // easy
-        Font::new(FontOptions::VGA, 0, 255, 0),
-        // medium
-        Font::new(FontOptions::VGA, 77, 166, 255),
-        // hard
-        Font::new(FontOptions::VGA, 255, 0, 0),
-    ];
+trait MatrixOperationen {
+    fn neu_einheit() -> Self;
+    fn multipliziere(self, other: &Mat) -> Mat;
+    fn transponiere(self) -> Mat;
+    fn runde(self, nk_stellen: i32) -> Mat;
+}
 
-    let mut gradient_calculator = gradient_calculator();
+impl MatrixOperationen for Mat {
+    fn neu_einheit() -> Self {
+        let mut mat: Mat = Mat::default();
+        for (i, element) in mat.iter_mut().enumerate() {
+            element[i] = 1.0;
+        }
 
-    let mut manager = FPS::new();
-    manager.framerate(20);
+        mat
+    }
+    // prüft nicht, ob die Matrizen multiplizierbar sind (alle sind quadratisch und von gleicher Größe)
+    fn multipliziere(self, other: &Mat) -> Mat {
+        let mut res: Mat = Mat::default();
+        let n = self.len();
 
-    // rng setup
-    let mut small_rng = SmallRng::seed_from_u64(
-        SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs(),
-    );
+        for i in 0..n {
+            for j in 0..n {
+                for (k, m2_zeile) in other.iter().enumerate() {
+                    res[i][j] += self[i][k] * m2_zeile[j];
+                }
+            }
+        }
 
-    // default to medium difficulty. Possible: 0-2
-    let mut difficulty: u8 = 1;
+        res
+    }
 
-    // game loop start
-    let mut restart_game = true;
-    while restart_game {
-        clear_screen(&screen);
+    fn transponiere(self) -> Mat {
+        let mut transponiert: Mat = Mat::default();
 
-        start_game_loop(
-            &screen,
-            &mut manager,
-            &fonts,
-            &mut gradient_calculator,
-            &mut small_rng,
-            &mut difficulty,
-        );
-        restart_game = gameover_handler();
+        for (i, zeile) in self.iter().enumerate() {
+            for (j, element) in zeile.iter().enumerate() {
+                transponiert[j][i] = *element;
+            }
+        }
+
+        transponiert
+    }
+
+    fn runde(self, nk_stellen: i32) -> Mat {
+        // die Rundung verändert nicht self, sondern erzeugt eine neue Matrix,
+        // damit spätere Rechnungen nicht auf gerundeten Werten basieren
+        let mut gerundet = Mat::default();
+        let fakt = 10f32.powi(nk_stellen);
+
+        for (i, zeile) in self.iter().enumerate() {
+            for (j, element) in zeile.iter().enumerate() {
+                // Gleitkommazahlen die 0 sein müssten können sehr kleine positive/negative Werte annehmen
+                // durch Rundungsfehler. Eine sehr kleine negative Zahl wird zu -0.0 gerundet,
+                // obwohl sie eigentlich 0 sein müsste. aus diesem Grund wird -0.0 zu 0.0 gerundet
+                gerundet[i][j] = match (*element * fakt).round() / fakt {
+                    f if f == -0.0 => 0.0,
+                    f => f,
+                };
+            }
+        }
+
+        gerundet
     }
 }
+
+fn main() {
+    let a = [
+        [1.0, 2.0, 3.0, 4.0],
+        [5.0, 6.0, 7.0, 6.0],
+        [9.0, 3.0, 11.0, 12.0],
+        [12.0, 1.0, 6.0, 7.0],
+    ];
+
+    // wird benutzt und verändert bei den Jacobi-Transformationen.
+    // a_jacobi ist nach der letzten Iteration bereits Aₕ, aber Aₕ wird danach noch einmal
+    // aus A und den U-Matrizen rekonstruiert (ebenso wird analog A rekonstruiert)
+    let mut a_jacobi = a;
+
+    print_mat("Matrix A", &a);
+
+    let mut u_matrizen: Vec<Mat> = Vec::new();
+
+    // Finde alle Elemente unterhalb der Nebendiagonale, die nicht 0 sind
+    // Alle Indexnamen (hier i0 und j0) enden auf 0, da Elemente ab 0 gezählt werden;
+    // nach Konvention ist bspw. i = i0 + 1
+    for (iteration, (i0, j0)) in nichtnull_unter_nebendiag(&a).iter().enumerate() {
+        println!(
+            "Element ({},{}) = {} ≠ 0.",
+            i0 + 1,
+            j0 + 1,
+            a_jacobi[*i0][*j0]
+        );
+        // Für jedes (i,j), welches 0 werden muss, erfolgt eine Jacobi-Transformation
+        // Speichere die U-Matrizen, um sie später für den Test zu verwenden
+        let u = jacobi_transform(&mut a_jacobi, *i0, *j0, iteration);
+        u_matrizen.push(u);
+    }
+
+    // (erneute) Erzeugung der Matrizen Aₕ und A
+    // Variable 'acc(umulator)' wird als Einheitsmatrix initialisiert (Mat::neu_einheit());
+    // durch "fold" erfolgt dann für jede Matrix "acc = acc * mat"
+    let u321_transponiert_multipliziert =
+        u_matrizen.iter().rev().fold(Mat::neu_einheit(), |acc, u| {
+            acc.multipliziere(&u.transponiere())
+        });
+    let u123_multipliziert = u_matrizen
+        .iter()
+        .fold(Mat::neu_einheit(), |acc, u| acc.multipliziere(u));
+
+    // Erzeugung von Aₕ
+    let mut ah_rekonstr_titel = String::from("Aₕ = ");
+    for i in (0..u_matrizen.len()).rev() {
+        ah_rekonstr_titel += &format!("U{}ᵀ * ", i + 1);
+    }
+    ah_rekonstr_titel += "A";
+    for i in 0..u_matrizen.len() {
+        ah_rekonstr_titel += &format!(" * U{}", i + 1);
+    }
+    let ah_rekonstr_mat = u321_transponiert_multipliziert
+        .multipliziere(&a)
+        .multipliziere(&u123_multipliziert);
+    print_mat(&ah_rekonstr_titel, &ah_rekonstr_mat.runde(4));
+
+    // Erzeugung von A
+    let mut a_rekonstr_titel = String::from("A = ");
+    for i in 0..u_matrizen.len() {
+        a_rekonstr_titel += &format!("U{} * ", i + 1);
+    }
+    a_rekonstr_titel += "Aₕ";
+    for i in (0..u_matrizen.len()).rev() {
+        a_rekonstr_titel += &format!(" * U{}ᵀ", i + 1);
+    }
+
+    let a_rekonstr_mat = u123_multipliziert
+        .multipliziere(&ah_rekonstr_mat)
+        .multipliziere(&u321_transponiert_multipliziert);
+    print_mat(&a_rekonstr_titel, &a_rekonstr_mat.runde(4));
+
+    // auf Windows schließt sich CMD sonst sofort
+    if cfg!(target_os = "windows") {
+        use std::io;
+        use std::io::prelude::*;
+        println!("Drücke eine beliebige Taste zum Beenden...");
+        let _ = io::stdin().read(&mut [0u8]).unwrap();
+    }
+}
+
+fn print_mat(name: &str, mat: &Mat) {
+    let hor_strich_breite = mat.len() * 10 - 1;
+
+    // Name gefolgt vom ersten horizontalen Trennstrich
+    println!("\x1b[32;1m{name}\x1b[0m:\n {:—<1$}", "", hor_strich_breite);
+
+    for zeile in mat {
+        print!("|");
+        for element in zeile {
+            // gebe jede Gleitkommazahl mit bis zu 7 Stellen aus,
+            // mittig in einem 7 Zeichen langen Feld; links und rechts immer ein Leerzeichen.
+            print!(" {:^7.7} |", element.to_string());
+        }
+        // Zeilenumbruch und horizontaler Trennstrich nach jeder Zeile
+        println!("\n {:—<1$}", "", hor_strich_breite);
+    }
+    // Zeilenumbruch nach Matrix
+    println!();
+}
+
+fn nichtnull_unter_nebendiag(mat: &Mat) -> Vec<(usize, usize)> {
+    let mut res = Vec::new();
+    let n = mat.len();
+
+    // alle Spalten außer den letzen beiden (da dort i nie > j+1)
+    for j0 in 0..n - 2 {
+        let i0_range = j0 + 2..n;
+        for i0 in i0_range {
+            if mat[i0][j0] != 0.0 {
+                res.push((i0, j0));
+            }
+        }
+    }
+
+    res
+}
+
+fn jacobi_transform(a: &mut Mat, i0: usize, j0: usize, iteration: usize) -> Mat {
+    // Rotationsindex
+    let (p0, q0) = (j0 + 1, i0);
+
+    let a_jp1j_wert = a[j0 + 1][j0];
+    let a_ij_wert = a[i0][j0];
+
+    let (cos_phi, sin_phi) = if a_jp1j_wert == 0.0 {
+        (0.0, 1.0)
+    } else {
+        (
+            a_jp1j_wert.abs() / (a_jp1j_wert.powi(2) + a_ij_wert.powi(2)).sqrt(),
+            (-(a_jp1j_wert.signum()) * a_ij_wert)
+                / (a_jp1j_wert.powi(2) + a_ij_wert.powi(2)).sqrt(),
+        )
+    };
+
+    println!("Eliminiere mit Φ ≈ {:.3}.", cos_phi.acos());
+
+    let u = ermittle_u(p0, q0, sin_phi, cos_phi);
+    print_mat(&format!("U{}", iteration + 1), &u);
+
+    *a = u.transponiere().multipliziere(a).multipliziere(&u);
+    u
+}
+
+fn ermittle_u(p0: usize, q0: usize, sin_phi: f32, cos_phi: f32) -> Mat {
+    let mut u: Mat = Mat::default();
+
+    // u_ii
+    for (i, zeile) in u
+        .iter_mut()
+        .enumerate()
+        .filter(|(i, _)| *i != p0 && *i != q0)
+    {
+        zeile[i] = 1.0;
+    }
+    // u_pp/u_qq
+    u[p0][p0] = cos_phi;
+    u[q0][q0] = cos_phi;
+
+    // u_pq
+    u[p0][q0] = sin_phi;
+
+    // u_qp
+    u[q0][p0] = -sin_phi;
+
+    // u_ij ist bereits 0.0
+
+    u
+}
+
+
+// fn main() {
+//     // screen setup
+//     let screen = ndless_sdl::init_default().expect("failed to set video mode");
+
+//     // used for score, color indicates difficulty
+//     let fonts = vec![
+//         // easy
+//         Font::new(FontOptions::VGA, 0, 255, 0),
+//         // medium
+//         Font::new(FontOptions::VGA, 77, 166, 255),
+//         // hard
+//         Font::new(FontOptions::VGA, 255, 0, 0),
+//     ];
+
+//     let mut gradient_calculator = gradient_calculator();
+
+//     let mut manager = FPS::new();
+//     manager.framerate(20);
+
+//     // rng setup
+//     let mut small_rng = SmallRng::seed_from_u64(
+//         SystemTime::now()
+//             .duration_since(SystemTime::UNIX_EPOCH)
+//             .unwrap()
+//             .as_secs(),
+//     );
+
+//     // default to medium difficulty. Possible: 0-2
+//     let mut difficulty: u8 = 1;
+
+//     // game loop start
+//     let mut restart_game = true;
+//     while restart_game {
+//         clear_screen(&screen);
+
+//         start_game_loop(
+//             &screen,
+//             &mut manager,
+//             &fonts,
+//             &mut gradient_calculator,
+//             &mut small_rng,
+//             &mut difficulty,
+//         );
+//         restart_game = gameover_handler();
+//     }
+// }
 
 // float math is way slower, but no problem for this beast of a machine
 fn gradient_calculator() -> impl FnMut(usize) -> Vec<u8> {
