59e7c983022a7b1c6242b641c0b7c71fe0dae9ef
Florian Schmidt <flo.schmidt10@gmail.com>
2023-09-21 15:26:27 +0200

    lol

--- src/main.rs
+++ src/main.rs
@@ -438,7 +438,8 @@ fn load_next_background(bg_idx: &mut usize) -> Option<Surface> {
             // dbg!(dir.by_ref().collect::<Vec<Result<DirEntry, ndless::io::Error>>>());
             let file_count = dir.by_ref().copied().count();
             dbg!(file_count);
-            let file_count = dir.by_ref().count();
+            
+            let file_count = fs::read_dir("/documents/backgrounds").iter().count();
             dbg!(file_count);
             let bg_file = dir.nth(*bg_idx).unwrap();
             match bg_file {
