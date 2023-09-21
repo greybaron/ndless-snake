45b764509d765eeff7946a94097b9c00db328699
Florian Schmidt <flo.schmidt10@gmail.com>
2023-09-21 15:13:56 +0200

    tttt

--- src/main.rs
+++ src/main.rs
@@ -435,6 +435,7 @@ fn load_next_background(bg_idx: &mut usize) -> Option<Surface> {
         Ok(mut dir) => {
             dbg!(&dir);
             dbg!(&bg_idx);
+            dbg!(dir.by_ref().collect());
             let file_count = dir.by_ref().count();
             let bg_file = dir.nth(*bg_idx).unwrap();
             match bg_file {
